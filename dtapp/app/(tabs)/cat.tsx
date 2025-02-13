402a4358c0a4f35d686832c61fa881508aa299ef
hardwayy <awesomedavexita@gmail.com>
2025-02-13 16:49:08 +0100

    addef giles

---
+++ dtapp/app/(tabs)/cat.tsx
@@ -0,0 +1,14 @@
+
+
+import React, { Component } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+const Cat = () => {
+  return <Text style={style.white}>Hello, I am your cat!</Text>;
+};
+const style= StyleSheet.create({
+    white:{
+        color: "#ffffff",
+        
+    }
+})
+export default Cat;
